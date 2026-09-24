07ab3feb4cdbfbdab1ac90c951db9392cd7b4ddb
Alvi-web0 <zulalvi947@gmail.com>
2026-09-24 18:39:20 +0700

    tambahkan fitur upload gambar dihalaman edit tamu

--- edit-tamu.php
+++ edit-tamu.php
@@ -38,8 +38,9 @@ if (isset($_GET['id'])) {
                 <h6>Data Tamu</h6>
             </div>
             <div class="card-body">
-                <form method="post" action="">
+                <form method="post" action="" enctype="multipart/form-data">
                     <input type="hidden" name="id_tamu" id="id_tamu" value="<?= $id_tamu?>">
+                    <input type="hidden" name="gambarLama" id="gambarLama" value="<?= $data['gambar']?>">
                     <div class="form-group row">
                         <label for="name-tamu" class="col-sm-3 col-form-label">Nama Tamu</label>
                         <div class="col-sm-8">
@@ -68,6 +69,13 @@ if (isset($_GET['id'])) {
                         <label for="kepentingan" class="col-sm-3 col-form-label">kepentingan</label>
                         <div class="col-sm-8">
                             <input type="text" class="form-control" id="kepentingan" name="kepentingan" value="<?= $data['kepentingan'] ?>">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="gambar" class="col-sm-3 col-form-label">Gambar Foto</label>
+                        <div class="col-sm-8">
+                            <img src="assets/upload_gambar/<?= $data['gambar']; ?>" alt="" width="30%">
+                            <input type="file" class="form-control-file" id="gambar" name="gambar">
                         </div>
                     </div>
                     <div class="form-group row">
